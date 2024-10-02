871b159d38eb0ed727634111336c6fc5533b8768
akhlus <samuellarcombe@gmail.com>
2024-10-02 18:45:07 +0100

    changed to single channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "NixOS configuration with two or more channels";
 
   inputs = {
-    nixpkgs-unstable.url = "nixpkgs/nixos-24.05";
     nixpkgs.url = "nixpkgs/nixos-unstable";
     home-manager.url="github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -12,19 +11,10 @@
     let
       system = "x86_64-linux";
       pkgs=nixpkgs.legacyPackages.${system};
-      overlay-unstable = final: prev: {
-        unstable = import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
-
-      };
     in {
       nixosConfigurations."s340" = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          # Overlays-module makes "pkgs.unstable" available in configuration.nix
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
           ./configuration.nix
         ];
       };
