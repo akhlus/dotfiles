4848bf9fa607cdce8bbce5f55c9d94904490d6c0
akhlus <samuellarcombe@gmail.com>
2025-01-29 01:08:51 +0000

    added kde

---
+++ de/kde.nix
@@ -0,0 +1,11 @@
+{
+  config,
+  pkgs,
+  pkgs-stable,
+  userSettings,
+  ...
+}: {
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+}
