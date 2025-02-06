0d5705f4300bcac6c4c66a2fae5cc014b89f4b41
akhlus <samuellarcombe@gmail.com>
2025-02-06 23:34:40 +0000

    29 current  2025-02-06 23:34:37  25.05.20250129.9d3ae80     6.13.0                          *

--- home/zed/zed.nix
+++ home/zed/zed.nix
@@ -8,12 +8,12 @@
   programs.zed-editor = {
     enable = true;
     extensions = import ./extensions.nix;
-    userSettings = lib.mkForce {
+    userSettings = {
       base_keymap = "VSCode";
-      buffer_font_family = "Zed Plex Mono";
-      ui_font_family = "Zed Plex Sans";
-      ui_font_size = 14;
-      buffer_font_size = 14;
+      buffer_font_family = lib.mkForce "Zed Plex Mono";
+      ui_font_family = lib.mkForce "Zed Plex Sans";
+      ui_font_size = lib.mkForce 14;
+      buffer_font_size = lib.mkForce 14;
       languages = import ./languages.nix;
       terminal = import ./terminal.nix;
     };
