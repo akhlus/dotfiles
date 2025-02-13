610756fa7b6a1c59d42afb8c751ee2973219f16f
akhlus <samuellarcombe@gmail.com>
2025-02-13 12:18:08 +0000

    63 current  2025-02-13 12:18:05  25.05.20250210.64e75cd  6.13.2                          *

--- programs/xournalpp.nix
+++ programs/xournalpp.nix
@@ -3,9 +3,5 @@
   home.file = {
     ".config/xournalpp/palette.gpl".source = ./files/palette.gpl;
     ".config/xournalpp/toolbar.ini".source = ./files/toolbar.ini;
-    ".config/xournalpp/settings.xml" = {
-      source = ./files/xournalpp-settings.xml;
-      recursive = true;
-    };
   };
 }
