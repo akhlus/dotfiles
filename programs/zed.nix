db7dd3e4c522d4a0a92edf39f6d7e312284704f5
akhlus <samuellarcombe@gmail.com>
2025-02-17 14:31:55 +0000

    2025-02-17-14-31 fixed zed lsp settings

--- programs/zed.nix
+++ programs/zed.nix
@@ -47,35 +47,34 @@
         };
       };
       lsp = {
-        programs.zed-editor.userSettings.lsp = {
-          /*
-          ruff = {
-            binary.path = "/run/current-system/sw/bin/ruff-lsp";
-            initialization_options = {
-              lineLength = 80;
-              lint = {extendSelect = ["I"];};
-            };
+        /*
+        ruff = {
+          binary.path = "/run/current-system/sw/bin/ruff-lsp";
+          initialization_options = {
+            lineLength = 80;
+            lint = {extendSelect = ["I"];};
           };
-          */
-          pylsp = {
-            binary.path = "/run/current-system/sw/bin/pylsp";
-          };
-          nixd = {
-            binary.path = "/run/current-system/sw/bin/nixd";
-            settings = {
-              formatting.command = ["alejandra" "--quiet"];
-              /*
-                 doesn't seem to work
-              options = {
-                home-manager.expr = "(builtins.getFlake \"${userSettings.flakePath}\").homeConfigurations.user.options";
-                flake-parts.expr = "(builtins.getFlake \"${userSettings.flakePath}\").currentSystem.options";
-                nixos.expr = "(builtins.getFlake \"${userSettings.flakePath}\").nixosConfigurations.system.options";
-              };
-              */
+        };
+        */
+        pylsp = {
+          binary.path = "/run/current-system/sw/bin/pylsp";
+        };
+        nixd = {
+          binary.path = "/run/current-system/sw/bin/nixd";
+          settings = {
+            formatting.command = ["alejandra" "--quiet"];
+            /*
+               doesn't seem to work
+            options = {
+              home-manager.expr = "(builtins.getFlake \"${userSettings.flakePath}\").homeConfigurations.user.options";
+              flake-parts.expr = "(builtins.getFlake \"${userSettings.flakePath}\").currentSystem.options";
+              nixos.expr = "(builtins.getFlake \"${userSettings.flakePath}\").nixosConfigurations.system.options";
             };
+            */
           };
         };
       };
+
       terminal = {
         dock = "bottom";
         working_directory = "current_project_directory";
