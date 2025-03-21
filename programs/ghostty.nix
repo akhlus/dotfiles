a493eb56e09c8de08c3df27b44130e49c5348b59
akhlus <samuellarcombe@gmail.com>
2025-03-21 05:31:51 +0000

    2025-03-21-05-31 hp rebuild

--- programs/ghostty.nix
+++ programs/ghostty.nix
@@ -8,13 +8,5 @@
       if settings.hostname == "penguin" || settings.hostname == "mba"
       then false
       else true;
-    settings = {
-      background-opacity = 0.95;
-      config-file = "?user-config";
-      font-family = "Source Code Pro";
-      font-size = 12;
-      theme = "akhlus";
-      window-theme = "ghostty";
-    };
   };
 }
