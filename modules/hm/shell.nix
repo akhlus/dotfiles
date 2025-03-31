aaff8dd55fc76a0b1983a013fc7d86ad607ddccf
akhlus <93236986+akhlus@users.noreply.github.com>
2025-03-31 17:01:35 +0100

    changed zsh config

--- modules/hm/shell.nix
+++ modules/hm/shell.nix
@@ -1,4 +1,4 @@
-{settings, ...}: let
+{settings, lib, pkgs, ...}: let
   alias = {
     "l" = "ls -a";
     "ll" = "ls -l";
@@ -20,6 +20,18 @@ in {
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh.enable = true;
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
   };
   programs.bash = {
     enable = true;
