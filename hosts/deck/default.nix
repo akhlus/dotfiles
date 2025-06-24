21f144481fbbd8abab6d21e5b7009f55bbac6069
akhlus <93236986+akhlus@users.noreply.github.com>
2025-06-24 11:05:27 +0100

    test

--- hosts/deck/default.nix
+++ hosts/deck/default.nix
@@ -2,14 +2,12 @@
   imports = [
     ./hardware.nix
   ];
-  options.customModules.system = {
-    options.customModules = {
-      de = {
-        enable = true;
-        environment = "gnome";
-        enableJovian = true;
-      };
-      steam.enable = true;
+  options.customModules = {
+    de = {
+      enable = true;
+      environment = "gnome";
+      enableJovian = true;
     };
+    steam.enable = true;
   };
 }
