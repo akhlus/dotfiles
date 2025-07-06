e781bad2be8333454a52d68e781a00e245513b1f
akhlus <93236986+akhlus@users.noreply.github.com>
2025-07-06 23:34:04 +0100

    applied ghostty patch

--- hosts/s340/home.nix
+++ hosts/s340/home.nix
@@ -2,5 +2,12 @@
   hMods = {
     zed.package = pkgs.zed-editor-fhs;
     vscode.package = pkgs.vscode-fhs;
+    ghostty.package = pkgs.ghostty.overrideAttrs (_: {
+      preBuild = ''
+        shopt -s globstar
+        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+        shopt -u globstar
+      '';
+    });
   };
 }
