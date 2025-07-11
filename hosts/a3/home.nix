b91bc395f035812d71ae1585cc08e2212468079b
akhlus <93236986+akhlus@users.noreply.github.com>
2025-07-11 13:39:17 +0100

    changed stuff

--- hosts/a3/home.nix
+++ hosts/a3/home.nix
@@ -1,4 +1,8 @@
 {pkgs, ...}: {
+  hMods = {
+    zed.package = pkgs.zed-editor-fhs;
+    vscode.package = pkgs.vscode-fhs;
+  };
   #used for machine specific config
   home.packages = with pkgs; [
     bambu-studio
--- hosts/s340/home.nix
+++ hosts/s340/home.nix
@@ -2,12 +2,5 @@
   hMods = {
     zed.package = pkgs.zed-editor-fhs;
     vscode.package = pkgs.vscode-fhs;
-    ghostty.package = pkgs.ghostty.overrideAttrs (_: {
-      preBuild = ''
-        shopt -s globstar
-        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
-        shopt -u globstar
-      '';
-    });
   };
 }
