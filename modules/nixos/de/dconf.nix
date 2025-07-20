a9ece97a99c9f2b4a4b13bc8857a90af54f27c7e
akhlus <93236986+akhlus@users.noreply.github.com>
2025-07-20 01:39:41 +0100

    2025-07-20-01-39 changed dconf

--- modules/nixos/de/dconf.nix
+++ modules/nixos/de/dconf.nix
@@ -16,7 +16,7 @@ with lib.hm.gvariant; {
     };
 
     "org/gnome/desktop/app-folders/folders/60d6a2f8-7774-4125-8d28-b157ec04dee7" = {
-      apps = ["onlyoffice-desktopeditors.desktop" "calibre-gui.desktop" "google-chrome.desktop" "com.mattjakeman.ExtensionManager.desktop" "code.desktop" "vlc.desktop" "org.gnome.TextEditor.desktop" "org.kicad.kicad.desktop" "chromium-browser.desktop" "firefox.desktop" "protonvpn-app.desktop" "anki.desktop" "org.kiwix.desktop.desktop" "org.qbittorrent.qBittorrent.desktop" "codium.desktop" "yt-dlg.desktop" "Vial.desktop" "startcenter.desktop" "mpv.desktop" "cursor.desktop" "bitwarden.desktop" "discord.desktop"];
+      apps = ["onlyoffice-desktopeditors.desktop" "calibre-gui.desktop" "google-chrome.desktop" "com.mattjakeman.ExtensionManager.desktop" "code.desktop" "vlc.desktop" "org.gnome.TextEditor.desktop" "org.kicad.kicad.desktop" "chromium-browser.desktop" "firefox.desktop" "protonvpn-app.desktop" "anki.desktop" "org.kiwix.desktop.desktop" "org.qbittorrent.qBittorrent.desktop" "codium.desktop" "yt-dlg.desktop" "Vial.desktop" "startcenter.desktop" "mpv.desktop" "cursor.desktop" "bitwarden.desktop" "discord.desktop" "openscad.desktop" "BambuStudio.desktop"];
       name = "Apps";
       translate = false;
     };
@@ -70,11 +70,12 @@ with lib.hm.gvariant; {
       cursor-theme = "Afterglow-Recolored-Catppuccin-Macchiato";
       color-scheme = "prefer-dark";
       document-font-name = "Inter Variable 12";
+      enable-animations = false;
       enable-hot-corners = false;
       font-name = "Inter Variable 12";
       gtk-theme = "Adwaita";
       icon-theme = "Adwaita";
-      monospace-font-name = "Source Code Pro 12";
+      monospace-font-name = "BlexMono Nerd Font 12";
     };
 
     "org/gnome/desktop/peripherals/mouse" = {
@@ -100,10 +101,13 @@ with lib.hm.gvariant; {
 
     "org/gnome/desktop/wm/keybindings" = {
       close = ["<Super>q"];
+      maximize = [];
+      unmaximize = [];
     };
 
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "appmenu:minimize,close";
+      focus-mode = "sloppy";
     };
 
     "org/gnome/epiphany" = {
@@ -112,8 +116,14 @@ with lib.hm.gvariant; {
 
     "org/gnome/mutter" = {
       dynamic-workspaces = true;
-      edge-tiling = true;
+      edge-tiling = false;
+      experimental-features = ["scale-monitor-framebuffer"];
       workspaces-only-on-primary = true;
+    };
+
+    "org/gnome/mutter/keybindings" = {
+      toggle-tiled-left = [];
+      toggle-tiled-right = [];
     };
 
     "org/gnome/nautilus/list-view" = {
@@ -145,7 +155,7 @@ with lib.hm.gvariant; {
 
     "org/gnome/shell" = {
       disabled-extensions = [];
-      enabled-extensions = ["clipboard-indicator@tudmotu.com" "blur-my-shell@aunetx" "dash-to-dock@micxgx.gmail.com" "caffeine@patapon.info" "appindicatorsupport@rgcjonas.gmail.com" "dash-to-panel@jderose9.github.com"];
+      enabled-extensions = ["clipboard-indicator@tudmotu.com" "blur-my-shell@aunetx" "dash-to-dock@micxgx.gmail.com" "caffeine@patapon.info" "appindicatorsupport@rgcjonas.gmail.com" "dash-to-panel@jderose9.github.com" "tilingshell@ferrarodomenico.com"];
       favorite-apps = ["org.gnome.Nautilus.desktop" "brave-browser.desktop" "dev.zed.Zed.desktop" "com.github.xournalpp.xournalpp.desktop" "spotify.desktop"];
       last-selected-power-profile = "power-saver";
       welcome-dialog-last-shown-version = "47.1";
@@ -219,6 +229,20 @@ with lib.hm.gvariant; {
       show-mounts = false;
       show-show-apps-button = true;
       show-trash = false;
+    };
+
+    "org/gnome/shell/extensions/tilingshell" = {
+      enable-window-border = true;
+      inner-gaps = mkUint32 2;
+      last-version-name-installed = "16.4";
+      layouts-json = "[{\"id\":\"Layout 2\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.24973958333333332,\"height\":0.24861111111111112,\"groups\":[1,4]},{\"x\":0.24973958333333332,\"y\":0,\"width\":0.5005208333333333,\"height\":0.5,\"groups\":[2,3,1]},{\"x\":0.7502604166666667,\"y\":0,\"width\":0.24973958333333335,\"height\":1,\"groups\":[2]},{\"x\":0.24973958333333332,\"y\":0.5,\"width\":0.5005208333333333,\"height\":0.5,\"groups\":[3,2,1]},{\"x\":0,\"y\":0.24861111111111112,\"width\":0.24973958333333332,\"height\":0.751388888888889,\"groups\":[4,1]}]},{\"id\":\"483365\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.5,\"height\":0.5,\"groups\":[1,2]},{\"x\":0.5,\"y\":0,\"width\":0.49999999999999994,\"height\":0.5,\"groups\":[3,1]},{\"x\":0,\"y\":0.5,\"width\":0.5,\"height\":0.5,\"groups\":[2,1]},{\"x\":0.5,\"y\":0.5,\"width\":0.49999999999999994,\"height\":0.49999999999999994,\"groups\":[3,1]}]},{\"id\":\"1186829\",\"tiles\":[{\"x\":0,\"y\":0,\"width\":0.24973958333333332,\"height\":0.5,\"groups\":[1,5]},{\"x\":0.24973958333333332,\"y\":0,\"width\":0.5005208333333333,\"height\":0.5,\"groups\":[2,3,1]},{\"x\":0.7502604166666667,\"y\":0,\"width\":0.2497395833333329,\"height\":0.5,\"groups\":[4,2]},{\"x\":0.24973958333333332,\"y\":0.5,\"width\":0.5005208333333333,\"height\":0.5,\"groups\":[3,2,1]},{\"x\":0.7502604166666667,\"y\":0.5,\"width\":0.2497395833333329,\"height\":0.49999999999999994,\"groups\":[4,2]},{\"x\":0,\"y\":0.5,\"width\":0.24973958333333332,\"height\":0.5,\"groups\":[5,1]}]}]";
+      outer-gaps = mkUint32 2;
+      overridden-settings = "{\"org.gnome.mutter.keybindings\":{\"toggle-tiled-right\":\"['<Super>Right']\",\"toggle-tiled-left\":\"['<Super>Left']\"},\"org.gnome.desktop.wm.keybindings\":{\"maximize\":\"['<Super>Up']\",\"unmaximize\":\"['<Super>Down', '<Alt>F5']\"},\"org.gnome.mutter\":{\"edge-tiling\":\"true\"}}";
+      selected-layouts = [["Layout 2"] ["Layout 2"]];
+      snap-assistant-animation-time = mkUint32 0;
+      tile-preview-animation-time = mkUint32 0;
+      top-edge-maximize = true;
+      window-border-color = "rgb(129,61,156)";
     };
 
     "org/gnome/shell/keybindings" = {
