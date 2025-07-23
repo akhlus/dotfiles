35e01cec3ab56c4dee5cd41b7c86094297f0cb82
akhlus <93236986+akhlus@users.noreply.github.com>
2025-07-23 03:58:52 +0100

    added chromebook host

--- hosts/penguin/home.nix
+++ hosts/penguin/home.nix
@@ -1,7 +1,21 @@
-{...}: {
+{pkgs, ...}: {
   xdg.configFile."systemd/user/cros-garcon.service.d/override.conf".text = ''
     [Service]
     Environment="PATH=%h/.nix-profile/bin:/usr/local/sbin:/usr/local/bin:/usr/local/games:/usr/sbin:/usr/bin:/usr/games:/sbin:/bin"
     Environment="XDG_DATA_DIRS=%h/.nix-profile/share:%h/.local/share:%h/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share"
   '';
+  hMods.packages = {
+    enableMinimal = false;
+    shell.enableZsh = false;
+    vscode = {
+      enable = true;
+      package = pkgs.vscode-fhs;
+    };
+    xournalpp.enable = true;
+  };
+  home.packages = with pkgs; [
+    bitwarden
+    nautilus
+    konsole
+  ];
 }
