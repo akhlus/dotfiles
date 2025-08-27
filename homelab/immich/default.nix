cb84d08d26400545989314abd39df5b4c1bb2d63
akhlus <93236986+akhlus@users.noreply.github.com>
2025-08-27 01:25:35 +0100

    fixed immich

--- homelab/immich/default.nix
+++ homelab/immich/default.nix
@@ -9,6 +9,8 @@ in {
     enableImmich = lib.mkEnableOption "Enable Immich" // {default = cfg.enableHomelab;};
   };
   config = (lib.mkIf cfg.enableImmich) {
-
-  };
+      programs.immich = {
+        enable = true;
+      };
+    };
 }
