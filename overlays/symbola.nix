ebcb9adaa69b980aae17e75b86c5294f729d31df
akhlus <93236986+akhlus@users.noreply.github.com>
2025-08-27 01:23:25 +0100

    removed old overlay

--- overlays/symbola.nix
+++
@@ -1,9 +0,0 @@
-self: super: {
-  symbola = super.symbola.overrideAttrs (oldAttrs: {
-    src = super.fetchzip {
-      url = "https://web.archive.org/web/20221006174450/https://dn-works.com/wp-content/uploads/2020/UFAS-Fonts/Symbola.zip";
-      stripRoot = false;
-      hash = "sha256-TsHWmzkEyMa8JOZDyjvk7PDhm239oH/FNllizNFf398=";
-    };
-  });
-}
