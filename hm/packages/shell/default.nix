3b62495761548f4a4b76851d37dded6dff70a47f
akhlus <93236986+akhlus@users.noreply.github.com>
2025-08-27 01:23:56 +0100

    changed to starship prompt

--- hm/packages/shell/default.nix
+++ hm/packages/shell/default.nix
@@ -28,33 +28,22 @@ in {
     enableBash = lib.mkEnableOption "Enable bash" // {default = true;};
   };
   config = {
-    programs.zsh = {
-      enable = cfg.enableZsh;
-      shellAliases = alias;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      oh-my-zsh.enable = true;
-      plugins = [
-        {
-          name = "powerlevel10k";
-          src = pkgs.zsh-powerlevel10k;
-          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-        }
-        {
-          name = "powerlevel10k-config";
-          src = lib.cleanSource ./p10k-config;
-          file = "p10k.zsh";
-        }
-      ];
-      initContent = ''
-        if [ -f $HOME/.zshrc-personal ]; then
-          source $HOME/.zshrc-personal
-        fi
-      '';
-    };
-    programs.bash = {
-      enable = cfg.enableBash;
-      shellAliases = alias;
+    programs = {
+      zsh = {
+        enable = cfg.enableZsh;
+        shellAliases = alias;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+        oh-my-zsh.enable = true;
+      };
+      bash = {
+        enable = cfg.enableBash;
+        shellAliases = alias;
+      };
+      starship = {
+        enable = true;
+        settings = builtins.fromTOML (builtins.readFile ./starship.toml);
+      };
     };
     home.shell = {
       enableBashIntegration = cfg.enableBash;
