b4a6666ffa4c73c53bc355e5d4dca2c3ae3bfc19
akhlus <93236986+akhlus@users.noreply.github.com>
2025-09-01 03:24:34 +0100

    2025-09-01-03-24 changed nix gc for nixos

--- nixos/system/nix.nix
+++ nixos/system/nix.nix
@@ -10,11 +10,7 @@
     nixPath = ["nixpkgs=${inputs.nixpkgs}"];
     gc = {
       automatic = true;
-      interval = {
-        Hour = 3;
-        Minute = 15;
-        Weekday = 7;
-      };
+      dates = "weekly";
       options = "-d";
     };
   };
